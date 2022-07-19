ad7b981a56fdc61ba15b3a500b96c1bcceeb0772
Dan Ostrovsky <dan.ostrovsky@gmail.com>
2022-07-19 14:01:26 -0400

    add derivationOrigin and recommend a different window size

--- src/auth_client_demo_assets/src/index.ts
+++ src/auth_client_demo_assets/src/index.ts
@@ -38,7 +38,10 @@ const init = async () => {
       windowOpenerFeatures: 
         `left=${window.screen.width / 2 - 200}, `+
         `top=${window.screen.height / 2 - 300},` +
-        `toolbar=0,location=0,menubar=0,width=400,height=600`
+        `toolbar=0,location=0,menubar=0,width=525,height=705`,
+      // See https://docs.nfid.one/migrating-domains
+      // for instructions on how to use derivationOrigin
+      // derivationOrigin: "https://<canister_id>.ic0.app"
     });
   };
 };
